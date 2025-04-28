456989a320ea24905552914765f7d9913a0fd9d0
sudkushal <sudarshankushal@gmail.com>
2025-04-28 21:56:43 +0530

    baseurl

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,8 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/ciportal/';
+    //public string $baseURL = 'http://localhost/ciportal/';
+    public string $baseURL = 'https://ciportal-c1fc18d75d54.herokuapp.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
