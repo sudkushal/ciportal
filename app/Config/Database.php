36881be895d69ff5390e199946cdd7342edab5fa
sudkushal <sudarshankushal@gmail.com>
2025-04-29 07:34:49 +0530

    composer changes

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,12 +24,12 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
-    /*public array $default = [
+    public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'root',
+        'username'     => '',
         'password'     => '',
-        'database'     => 'hdc',
+        'database'     => '',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
@@ -48,33 +48,7 @@ class Database extends Config
             'date'     => 'Y-m-d',
             'datetime' => 'Y-m-d H:i:s',
             'time'     => 'H:i:s',
-        ],*/
-
-        public array $default = [
-            'DSN'          => '',
-            'hostname'     => 'clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com	',
-            'username'     => 'pbkglcyp0udcr23t',
-            'password'     => 'ye52hoygwbswbf13',
-            'database'     => 'vyzq7ajxcubx1ndl',
-            'DBDriver'     => 'MySQLi',
-            'DBPrefix'     => '',
-            'pConnect'     => false,
-            'DBDebug'      => true,
-            'charset'      => 'utf8mb4',
-            'DBCollat'     => 'utf8mb4_general_ci',
-            'swapPre'      => '',
-            'encrypt'      => false,
-            'compress'     => false,
-            'strictOn'     => false,
-            'failover'     => [],
-            'port'         => 3306,
-            'numberNative' => false,
-            'foundRows'    => false,
-            'dateFormat'   => [
-                'date'     => 'Y-m-d',
-                'datetime' => 'Y-m-d H:i:s',
-                'time'     => 'H:i:s',
-            ],
+        ],
     ];
 
     //    /**
